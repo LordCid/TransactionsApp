dfacc73907f2d55da6a4191b439ab8ba9c173843
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-09-23 15:27:53 +0200

    Error dialog added

---
+++ app/src/main/java/com/albertcid/transactionsapp/presentation/ErrorDialogFragment.kt
@@ -0,0 +1,19 @@
+package com.albertcid.transactionsapp.presentation
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.albertcid.transactionsapp.R
+
+class ErrorDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            return AlertDialog.Builder(it)
+                .setMessage(R.string.download_error_message)
+                .setNegativeButton(R.string.ok_label) { _, _ -> dismiss() }
+                .create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
