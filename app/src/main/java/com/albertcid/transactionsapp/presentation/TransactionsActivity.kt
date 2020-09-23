c3b4c1e186cefc900fdad9a748ac03e26d5e8e6e
ALBERTPORTATIL\Albert <cidalbert@hotmail.com>
2020-09-23 15:26:39 +0200

    Transitions activity implementation

--- app/src/main/java/com/albertcid/transactionsapp/presentation/TransactionsActivity.kt
+++ app/src/main/java/com/albertcid/transactionsapp/presentation/TransactionsActivity.kt
@@ -3,19 +3,42 @@ package com.albertcid.transactionsapp.presentation
 import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.albertcid.transactionsapp.R
+import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class TransactionsActivity : AppCompatActivity() {
 
+    @Inject
     lateinit var viewModelFactory: ViewModelProvider.NewInstanceFactory
     private lateinit var viewModel: TransactionsViewModel
     private lateinit var progressDialog: ProgressDialog
 
+    private lateinit var transactionsAdapter: TransactionsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        transactionsAdapter = TransactionsAdapter(DateFormat.getDateFormat(this))
+        setUpUI()
+        setViewModel()
+    }
+
+    private fun setUpUI() {
+        progressDialog = ProgressDialog(this)
+        listView.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            this.adapter = transactionsAdapter
+        }
+
     }
 
     private fun setViewModel() {
@@ -29,15 +52,22 @@ class TransactionsActivity : AppCompatActivity() {
 
     private fun updateUI(screenState: TransactionsViewState) {
         when (screenState) {
-            is TransactionsViewState.Loading -> {}
+            is TransactionsViewState.Loading -> showLoadingDialogFragment()
             is TransactionsViewState.ShowData -> {
 
             }
-            is TransactionsViewState.Error -> {
-
-            }
+            is TransactionsViewState.Error -> showErrorDialogFragment()
         }
     }
+    private fun showLoadingDialogFragment() {
+        progressDialog.setMessage(getString(R.string.downloading_title_dialog))
+        progressDialog.show()
+    }
 
+    private fun showErrorDialogFragment() {
+        progressDialog.dismiss()
+        val errorDialog = ErrorDialogFragment()
+        errorDialog.show(supportFragmentManager, "error")
+    }
 
 }
